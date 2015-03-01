c90c43d2f08dcb395ba1d536696cd2722c37e025
Purav Shah <puravshah23@gmail.com>
2015-02-28 22:31:34 -0500

    Replace viewpager with material design type nav bar

--- app/src/main/java/com/notdecaf/foodinder/MainActivity.java
+++ app/src/main/java/com/notdecaf/foodinder/MainActivity.java
@@ -1,37 +1,103 @@
 package com.notdecaf.foodinder;
 
+import android.support.v4.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 	MainPagerAdapter mainPagerAdapter;
-	//FOR SLACK!!!!
+	DrawerLayout mDrawerLayout;
+	ActionBarDrawerToggle mDrawerToggle;
+	ListView mDrawerList;
+	ArrayList<String> drawerItems;
+	ProfileFragment profileFragment;
+	HomeFragment homeFragment;
+	HistoryFragment historyFragment;
+	ArrayAdapter drawerListAdapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		List<Fragment> fragmentList = new ArrayList<Fragment>();
-		fragmentList.add(new ProfileFragment());
-		fragmentList.add(new HomeFragment());
-		fragmentList.add(new HistoryFragment());
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		Toolbar mToolBar = (Toolbar) findViewById(R.id.toolbar);
+		mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolBar,
+				R.string.app_name, R.string.app_name);
+		drawerItems = new ArrayList<String>();
 
-		ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
-		mainPagerAdapter = new MainPagerAdapter(getSupportFragmentManager(),fragmentList);
-		pager.setAdapter(mainPagerAdapter);
+		drawerItems.add("Profile");
+		drawerItems.add("History");
 
-		//Start off with the home fragment
-		pager.setCurrentItem(1);
+		//Set the beginning page as the home fragment
+		homeFragment = new HomeFragment();
+		profileFragment = new ProfileFragment();
+		historyFragment = new HistoryFragment();
+
+		getSupportFragmentManager().beginTransaction().add(R.id.main_content, homeFragment).commit();
+
+		//Creating the toolbar that will hold the spinning drawer icon
+		setSupportActionBar(mToolBar);
+
+		//Set the toggle for the drawer which indicates the state of the drawer and spins the icon
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		mDrawerToggle.syncState();
+
+		//Populate the drawer
+		drawerListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+				drawerItems);
+		mDrawerList.setAdapter(drawerListAdapter);
+
+		mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+				String selected = drawerItems.get(position);
+				if (selected.equals("Profile")) {
+					drawerItems.clear();
+					String [] items = getResources().getStringArray(R.array.profile_list);
+					drawerItems.addAll(Arrays.asList(items));
+					drawerListAdapter.notifyDataSetChanged();
+					transaction.replace(R.id.main_content,profileFragment);
+					transaction.commit();
+				} else if (selected.equals("History")){
+					drawerItems.clear();
+					String [] items = getResources().getStringArray(R.array.history_list);
+					drawerItems.addAll(Arrays.asList(items));
+					drawerListAdapter.notifyDataSetChanged();
+					transaction.replace(R.id.main_content,historyFragment);
+					transaction.commit();
+				} else {
+					drawerItems.clear();
+					String [] items = getResources().getStringArray(R.array.home_list);
+					drawerItems.addAll(Arrays.asList(items));
+					drawerListAdapter.notifyDataSetChanged();
+					transaction.replace(R.id.main_content,homeFragment);
+					transaction.commit();
+				}
+			}
+		});
 	}
 
 
@@ -53,8 +119,32 @@ public class MainActivity extends ActionBarActivity {
 		if (id == R.id.action_settings) {
 			return true;
 		}
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public void onBackPressed() {
+		if(mDrawerLayout.isDrawerOpen(Gravity.START|Gravity.LEFT)){
+			mDrawerLayout.closeDrawers();
+			return;
+		}
+		super.onBackPressed();
 	}
 
 	private class MainPagerAdapter extends FragmentPagerAdapter {
